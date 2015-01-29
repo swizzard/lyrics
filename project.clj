63041ce937962b74fc167065bc2cfe7940aceec7
Sam Raker <sam.raker@gmail.com>
2015-01-29 15:20:11 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject lyrics "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.3.5"]
+                 [clj-http "1.0.1"]
+                 [enlive "1.1.5"]]
+  :plugins [[lein-gorilla "0.3.4"]])
