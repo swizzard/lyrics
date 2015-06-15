a869aab826a3d9047ffe82178e4bd5e26bcec0f2
Sam Raker <sam.raker@gmail.com>
2015-06-15 16:49:08 -0400

    refactor lyrics-to-neo4j to operate over a seq of letters

--- src/lyrics/core.clj
+++ src/lyrics/core.clj
@@ -6,13 +6,14 @@
 
 (defn lyrics-to-neo4j
   "Retrieve lyrics, parse them, and upload them to Neo4j"
-  ([conn artists]
-    (doseq [l (scraping/get-all-lyrics artists)]
+  ([conn letters]
+    (doseq [l (scraping/get-all-lyrics letters)]
       (if-let [parsed (parsing/parse-blob l)]
-           (neo4j/parse-song conn parsed))))
-  ([conn] (lyrics-to-neo4j (scraping/get-all-artists))))
+        (neo4j/parse-song conn parsed)))))
 
 (defn -main
   "Main function"
   []
-  (lyrics-to-neo4j (neo4j/get-conn)))
+  (lyrics-to-neo4j (neo4j/get-conn) ["1" "a" "b" "c" "d" "e" "f" "g" "h" "i"
+                                     "j" "k" "l" "m" "n" "o" "p" "q" "r" "s"
+                                     "t" "u" "v" "w" "x" "y" "z"]))
